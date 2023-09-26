3cd0a6ea66bd24c584e2ff724e1acc650f94e108
Keren_Apuque <91158924+KeReN995221@users.noreply.github.com>
2023-09-26 20:58:29 -0300

    Update lista_encadeada.cpp

--- lista_encadeada.cpp
+++ lista_encadeada.cpp
@@ -1,7 +1,6 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
+#include <string.h>
 
 
 typedef struct no{
@@ -129,30 +128,62 @@ void remover (No **lista, int valor){
 
 }
 
+void contadorLista (No *aux){
+	int contador;
+	while(aux){
+		contador++;
+		aux = aux->proximo;
+	}
+	printf("\n existem %d elementos nessa lista", contador);
+}
+
 int main(){
 
 	No *lista=NULL;
-	int opcao; 
-	while (while opcao != 5){
-	    printf("Menu de opção: \n1. Inserir um elemento no começo da lsita\n2. Inserir um elemento no meio da lista\n3. Inserir um novo elemento no fim\n4. Remover um elemento \n5. Sair\nOpção: ");
+	int opcao, valor; 
+	while (opcao != 6){
+	    printf("\n--------Menu de opção--------- \n1. Inserir um elemento no começo da lsita\n2. Inserir um elemento no meio da lista\n3. Inserir um novo elemento no fim\n4. Remover um elemento \n5. Saber quantos elementos tem na lista\n6. Sair\nOpção: ");
 	    scanf("%d", &opcao);
-	}
-	imprimirLista(lista);
-	adicionarInicio(&lista, 10);
-	imprimirLista(lista);
-	adicionarInicio(&lista, 20);
-	imprimirLista(lista);
-	adicionarInicio(&lista, 50);
-	imprimirLista(lista);
-	adicionarFim(&lista, 5);
-	imprimirLista(lista);
-	adicionarFim(&lista, 1);
-	imprimirLista(lista);
-	adicionarInicio(&lista, 100);
-	imprimirLista(lista);
-	remover(&lista, 50);
-	imprimirLista(lista);
+	     if( opcao == 5){
+	    	printf("\nSaindo, até logo");
+	    	break;
+		}
+	    printf("\n------------------------------------");
+	   
+	    printf("\nInforme o valor que deseja adicionar ou remover: ");
+	    scanf("%d", &valor);
+	    
+	    switch (opcao){
+	    	case 1:
+		    	adicionarInicio(&lista, valor);
+		    	imprimirLista(lista);
+		    	printf("\nElemento adicionado");
+			break; 
+			case 2:
+				int valorAuxiliar; 
+				printf("\nInforme o número para servir de parâmetro:  ");
+				scanf("%d", &valorAuxiliar);
+				adicionarMeio(&lista, valor, valorAuxiliar);
+				imprimirLista(lista);
+				printf("\nElemento adicionado");
+			break; 
+			case 3: 
+				adicionarFim(&lista, valor);
+				imprimirLista(lista);
+				printf("\nElemento adicionado");
+			break; 
+			case 4: 
+				remover(&lista, valor);
+				imprimirLista(lista);
+				printf("\nElemento excluido!");
+			break;
+			case 5: 
+				contadorLista(lista);
+			break;
+			
+		}
+	    
+	}	
 	
 	return 0;
 }
-
